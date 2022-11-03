2e1d02b765ff94748a5d2b13112f8a898c6eeb96
alfsuace <115726503+alfsuace@users.noreply.github.com>
2022-11-03 17:00:50 +0100

    Creación de "Usuario"

---
+++ src/com/alfsuace/biblioteca/assets/Usuario.java
@@ -0,0 +1,75 @@
+package com.alfsuace.biblioteca.assets;
+
+public class Usuario {
+    //-id: Integer
+    //-nombre: String
+    //-apellidos: String
+    //-dni: String
+    //-telefono: Integer
+    //-direccion: String
+    //-población: String
+
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private Integer telefono;
+    private String direccion;
+    private String poblacion;
+
+    public void setId (Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId () {
+        return id;
+    }
+
+    public void setNombre (String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre () {
+        return nombre;
+    }
+
+    public void setApellidos (String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getApellidos () {
+        return apellidos;
+    }
+
+    public void setDni (String dni) {
+        this.dni = dni;
+    }
+
+    public String getDni () {
+        return dni;
+    }
+
+    public void setTelefono (Integer telefono) {
+        this.telefono = telefono;
+    }
+
+    public Integer getTelefono () {
+        return telefono;
+    }
+
+    public void setDireccion (String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion () {
+        return direccion;
+    }
+
+    public void setPoblacion (String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public String getPoblacion () {
+        return poblacion;
+    }
+}
